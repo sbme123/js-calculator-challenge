f05dc6803dde273ab90010375dad1861de4c77b6
sbme23 <93839730+sbme23@users.noreply.github.com>
2021-12-31 14:50:25 +0000

    Updated button event listener to target the result output variable

--- script.js
+++ script.js
@@ -2,15 +2,35 @@
 let result = document.getElementById("result")
 
 // get the value of the buttons 
-buttons = document.querySelectorAll("button")
+buttons = document.querySelectorAll(".button")
 
 // Create a variable to store the new value
 let resultAnswer = ""
+
+
 
 // Add an event listener and a for of loop to get the button value
 
 for (item of buttons) {
     item.addEventListener("click", (event) => {
-        console.log("Test")
+        buttonValue = event.target.innerText
+        console.log(`The value of this button is ${buttonValue}`)
+        result.innerHTML += buttonValue
+        
+        // if (buttonValue == "x") {
+        //     buttonValue = "*"
+        //     resultAnswer.innerHtml += buttonValue
+        //     result.innerHtml = resultAnswer
+        // }
     })
+    
 }
+
+// if statement to use the button value for the calculations
+
+// if (buttonValue == "x") {
+//     buttonValue = "*"
+//     resultAnswer += buttonValue
+//     result.value - resultAnswer
+// }
+
