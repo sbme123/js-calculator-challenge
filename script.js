af46bf26a9b169888a4b717a85873b86613e7648
sbme23 <93839730+sbme23@users.noreply.github.com>
2021-12-31 16:09:05 +0000

    Restarting js
    Couldn't get my approach to work. Talking a break and rethinking how to calculate.

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-// Get the calculator result to store a new value
+
 let resultOnScreen = document.getElementById("result")
-
-// get the value of the buttons 
-buttons = document.querySelectorAll(".button")
-
-// Create a variable to store the new value
-let resultAnswer = ""
+console.dir(resultOnScreen)
 
 
 
-// Add an event listener and a for of loop to get the button value
 
-for (item of buttons) {
-    item.addEventListener("click", (event) => {
-        // getting the value of each button
-        buttonValue = event.target.innerText
-        console.log(`The value of this button is ${buttonValue}`)
-        result.innerText += buttonValue
 
-    // if statement to perform multiple calc operations starting with the delete button
-        if (buttonValue == "AC") {
-            result.innerText = ""
-        // } else if (buttonValue == "*") {
-        //     resultAnswer += buttonValue 
-        //     result.value = resultAnswer
-            // Using the JS eval function to calculate the resultAnswer varliable 
-        } 
-    })
+
+
+
+
+
+// // Get the calculator result to store a new value
+// let resultOnScreen = document.getElementById("result")
+
+// // get the value of the buttons 
+// buttons = document.querySelectorAll(".button")
+
+// // Create a variable to store the new value
+// let resultAnswer = ""
+
+
+
+// // Add an event listener and a for of loop to get the button value
+
+// for (item of buttons) {
+//     item.addEventListener("click", (event) => {
+//         // getting the value of each button
+//         buttonValue = event.target.value
+//         console.log(`The value of this button is ${buttonValue}`)
+//         resultOnScreen.innerText = buttonValue.value
+//         console.log(resultOnScreen)
+
+//     // if statement to perform multiple calc operations starting with the delete button
+//         if (buttonValue == "AC") {
+//             resultOnScreen.innerText = ""
+//         // } else if (buttonValue == "*") {
+//         //     resultAnswer += buttonValue 
+//         //     result.value = resultAnswer
+//             // Using the JS eval function to calculate the resultAnswer varliable 
+//         } else if (buttonValue = "=") {
+//             resultOnScreen = eval(resultAnswer)
+//         }   else {
+            
+//         }
+//     })
     
-}
+// }
