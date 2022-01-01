a74ef7b303329109ced8e055d4263dc3bcaa1ef2
sbme23 <93839730+sbme23@users.noreply.github.com>
2022-01-01 19:06:30 +0000

    Updating script.js
    Added the results and buttonsClicked variables.  Started a switch statement and added the default action. I need to add a function to be called as part of the button event listener.

--- script.js
+++ script.js
@@ -3,21 +3,34 @@
 // resultsOnScreen will display the final calculation to the user. 
 const resultsOnScreen = document.getElementById("result-on-screen")
 
+// result variable to store the final calculation before it is passed to the resultsOnScreen variable above 
+// buttonsClicked variable to store what buttons the users has clicked as a string to later be changed to a number value. 
+let result = ""
+let buttonsClicked = ""
+
 // Variable to select the calculator buttons 
 let calcButton = document.querySelectorAll("button")
 
-// Event listener to obtain the button values 
+// Event listener to obtain the button values and pass this to the resultsOnScreen variable  
 
 calcButton.forEach(button => {
     button.addEventListener("click", (event) => {
         const buttonValue = event.target.value
         resultsOnScreen.innerHTML = buttonValue
+    
     })
     
 });
 
+/* ---- NEXT ---- */ 
 
- 
+// Switch case to check for the button values to append and then eval the results 
+
+switch(buttonValue) {
+
+    default: 
+    buttonsClicked += buttonValue
+}
 
 
 
