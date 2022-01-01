281735f87edbb92873e5667793cdabae1e5936c6
sbme23 <93839730+sbme23@users.noreply.github.com>
2022-01-01 18:47:22 +0000

    Updated script.js
    Now Then! Third time is the charm! Added the resultOnScreen & calcButton variables.  Also added the forEAch Button loop to grab those button values that will be displayed on the screen.

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+/* Almost tearing my hear out trying to make if statements work. I've had much more success with a switch case. I struggled to find out how to */
 
+// resultsOnScreen will display the final calculation to the user. 
+const resultsOnScreen = document.getElementById("result-on-screen")
+
+// Variable to select the calculator buttons 
+let calcButton = document.querySelectorAll("button")
+
+// Event listener to obtain the button values 
+
+calcButton.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const buttonValue = event.target.value
+        resultsOnScreen.innerHTML = buttonValue
+    })
+    
+});
 
 
  
