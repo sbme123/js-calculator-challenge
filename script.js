94969fdbbc3afd6233f3d5cded589e512e82a95d
sbme23 <93839730+sbme23@users.noreply.github.com>
2022-01-01 12:44:06 +0000

    Added Calculate Function
    Attempted to run a sum calculate function to add two numbers together. However the displayOnScreen is not recognizing the operator buttons. I need to work out how to separate the number and the operator button values.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Setting up the result displayed on the calculator, this will display the calculator result. 
+// 1. Setting up the result displayed on the calculator, this will display the calculator result. 
 const resultOnScreen = document.getElementById("result-on-screen")
 /*
 Testing that it's working 
@@ -6,7 +6,7 @@ resultOnScreen.innerHTML = 1
 console.log(resultOnScreen.innerHTML)
 */
 
-// Adding a qeury selector for the buttons, everything with the button class
+// 2. Adding a qeury selector for the buttons, everything with the button class
 calcButtons = document.querySelectorAll(".button")
 /* 
 Testing that It's working 
@@ -21,16 +21,54 @@ buttons.forEach(button => {
 }); 
 */
 
-// The resultOnScreen needs to change with the buttons, need to target the inner HTML of the resultOnScreen. 
-// This can be done by using a for loop with the event listener 
+// 4. May need to add some sort of regex functionality for the symbols. 
+// Create a variable for the symbol an check if it includes this within the calculation.  
+
+const OperatorButtonArr = ['AC', '±', '%', '÷', 'X', '-', '+', '='] 
+
+
+// 5. Add an empty arrays that will changing depending on the user input 
+// These need to work as a string to concatinate. Need to convert this back to a number value.  
+let firstValue = ''
+let secondValue = ''
+let operatorButton = ''
+let result = ''
+
+// 6. Need to add a calculate function tha will take the number value as a number with a switch case or an if statement. 
+// Let's try a simple sum caclulation 
+
+const calculate = () => {
+    firstValue.parseFloat(firstValue)
+    secondValue.parseFloat(secondValue)
+
+    if (buttonValue === '+') {
+        result = firstValue = secondValue
+        resultOnScreen.innerHTML = result
+    }
+
+}
+
+
+
+// 3. The resultOnScreen needs to change with the buttons, need to target the inner HTML of the resultOnScreen. 
+// This can be done by using a for loop with the event listener function 
 
 for (button of calcButtons) {
     button.addEventListener("click", (event) => {
         const buttonValue = event.target.innerText
         resultOnScreen.innerText += buttonValue
-        
+        // Need to research how to seperate the symboles. 
     })
 }
+
+
+ 
+
+
+
+
+
+
 
 
 
